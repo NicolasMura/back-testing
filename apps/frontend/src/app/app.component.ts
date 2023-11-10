014b8ed4761e4730c3fa8c77b48b9005a696094d
NicolasMura <contact@nicolasmura.fr>
2023-11-10 19:15:03 +0100

    Add investment, startDate and endDate to form

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,8 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, inject } from '@angular/core';
-import { EChartsOption } from 'echarts';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  AbstractControl,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { ECharts, EChartsOption } from 'echarts';
+import { DateTime } from 'luxon';
 import { NgxEchartsModule } from 'ngx-echarts';
-import { Observable, catchError, from, map, switchMap } from 'rxjs';
+import {
+  Observable,
+  catchError,
+  from,
+  map,
+  pairwise,
+  startWith,
+  switchMap,
+} from 'rxjs';
 import { Sheet2JSONOpts, WorkSheet, read, utils } from 'xlsx';
 
 type DataT = {
@@ -15,32 +34,107 @@ export interface RawLine {
   C: string;
 }
 
+interface Investment {
+  name: string;
+  code: string;
+  isinCode: string;
+  sheetname: string;
+}
+
 @Component({
   standalone: true,
-  imports: [NgxEchartsModule],
+  imports: [CommonModule, ReactiveFormsModule, NgxEchartsModule],
   selector: 'back-testing-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  private readonly destroy: DestroyRef = inject(DestroyRef);
   httpClient = inject(HttpClient);
+  nnfb = inject(NonNullableFormBuilder);
+
   title = 'frontend';
+  chartInstance!: ECharts;
   initOpts = {
     renderer: 'canvas',
     animation: 'linear',
   };
   options0!: EChartsOption;
   updateOptions0!: EChartsOption;
-  loading = true;
+  loading!: boolean;
+
+  investments: Investment[] = [
+    {
+      name: 'Lyxor PEA Monde (MSCI World) UCITS ETF - ELWD',
+      code: 'ELWD',
+      isinCode: 'FR0011869353',
+      sheetname:
+        'assets/Historique des VLs_Lyxor PEA Monde (MSCI World) UCITS ETF - Capi._FR0011869353_13_05_2014.xlsx',
+    },
+    {
+      name: 'Lyxor MSCI World UCITS ETF - Dist - WLD',
+      code: 'LWD',
+      isinCode: 'FR0010315770',
+      sheetname:
+        'assets/Historique de performance_Lyxor MSCI World UCITS ETF - Dist_FR0010315770_27_07_2023.xlsx',
+    },
+    {
+      name: 'Amundi PEA Monde (MSCI World) UCITS ETF - CW8',
+      code: 'CW8',
+      isinCode: 'LU1681043599',
+      sheetname:
+        'assets/Historique des VLs_Amundi MSCI World UCITS ETF - EUR (C)_LU1681043599_18_04_2018.xlsx',
+    },
+  ];
+  startDates: string[] = [];
+  endDates: string[] = [];
+  form = this.nnfb.group({
+    investment: [this.investments[0]],
+    startDate: [new Date().toISOString().split('T')[0]],
+    endDate: [new Date().toISOString().split('T')[0]],
+    initialCapital: [
+      1000,
+      [
+        Validators.required,
+        CustomValidators.integer(),
+        CustomValidators.strictlyPositive,
+      ],
+    ],
+    monthlyAmount: [350, [Validators.required, CustomValidators.integer()]],
+  });
 
   private data!: DataT[];
 
   ngOnInit(): void {
     // initialize chart
+    this.initChart();
+
+    this.loadSimulation(this.form.value.investment as Investment);
+
+    this.form.valueChanges
+      .pipe(startWith(null), pairwise(), takeUntilDestroyed(this.destroy))
+      .subscribe(([prev, curr]) => {
+        if (prev && curr && prev.investment !== curr.investment) {
+          this.resetChart(curr.investment as Investment);
+        }
+        if (prev && curr && prev.startDate !== curr.startDate) {
+          this.updateEndDates(curr.startDate as string);
+        }
+        if (prev && curr && prev.endDate !== curr.endDate) {
+          this.updateStartDates(curr.endDate as string);
+        }
+      });
+  }
+
+  onChartInit(e: ECharts) {
+    this.chartInstance = e;
+  }
+
+  initChart(): void {
     this.data = [];
     this.options0 = {
       title: {
-        text: 'Prix de Lyxor MSCI World - EWLD',
+        text: `Prix de ${this.form.value.investment?.name}`,
       },
       legend: {},
       tooltip: {
@@ -76,21 +170,47 @@ export class AppComponent implements OnInit {
       },
       series: [
         {
-          name: 'EWLD',
+          name: this.form.value.investment?.code,
           type: 'line',
           showSymbol: false,
           data: this.data,
         },
       ],
     };
+  }
+
+  resetChart(newInvestment: Investment): void {
+    this.chartInstance.clear();
+    this.initChart();
+    this.loadSimulation(newInvestment);
+  }
+
+  updateEndDates(newStartDate: string): void {
+    this.endDates = this.data
+      .filter(
+        (d) =>
+          DateTime.fromISO(d.value[0]) >
+          DateTime.fromISO(newStartDate as string)
+      )
+      .map((d) => d.value[0])
+      .sort();
+  }
+
+  updateStartDates(newEndDate: string): void {
+    this.startDates = this.data
+      .filter(
+        (d) =>
+          DateTime.fromISO(d.value[0]) < DateTime.fromISO(newEndDate as string)
+      )
+      .map((d) => d.value[0])
+      .sort();
+  }
+
+  loadSimulation(investment: Investment): void {
+    this.loading = true;
 
     this.httpClient
-      .get(
-        'assets/Historique des VLs_Lyxor PEA Monde (MSCI World) UCITS ETF - Capi._FR0011869353_13_05_2014.xlsx',
-        // 'assets/Historique de performance_Lyxor MSCI World UCITS ETF - Dist_FR0010315770_27_07_2023.xlsx',
-        // 'assets/Historique des VLs_Amundi MSCI World UCITS ETF - EUR (C)_LU1681043599_18_04_2018.xlsx',
-        { responseType: 'blob' }
-      )
+      .get(investment.sheetname, { responseType: 'blob' })
       .pipe(
         catchError((err) => {
           console.error(err);
@@ -102,6 +222,7 @@ export class AppComponent implements OnInit {
         map((rawLines) => this.mapRawLinesToIndiceData(rawLines))
       )
       .subscribe((data) => {
+        this.data = data;
         this.loading = false;
         this.updateOptions0 = {
           series: [
@@ -110,56 +231,16 @@ export class AppComponent implements OnInit {
             },
           ],
         };
+        this.startDates = data.map((d) => d.value[0]).sort();
+        this.endDates = [...this.startDates];
+        this.startDates.pop();
+        this.endDates.shift();
+
+        this.form.patchValue({
+          startDate: this.startDates[0],
+          endDate: this.endDates[this.startDates.length - 1],
+        });
       });
-  }
-
-  // Example
-
-  options1: EChartsOption = {
-    legend: {},
-    tooltip: {},
-    dataset: {
-      // Provide a set of data.
-      source: [
-        ['product', '2015', '2016', '2017'],
-        ['Matcha Latte', 43.3, 85.8, 93.7],
-        ['Milk Tea', 83.1, 73.4, 55.1],
-        ['Cheese Cocoa', 86.4, 65.2, 82.5],
-        ['Walnut Brownie', 72.4, 53.9, 39.1],
-      ],
-    },
-    // Declare an x-axis (category axis).
-    // The category map the first column in the dataset by default.
-    xAxis: { type: 'category' },
-    // Declare a y-axis (value axis).
-    yAxis: {},
-    // Declare several 'bar' series,
-    // every series will auto-map to each column by default.
-    series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }],
-  };
-
-  mergeOptions1!: EChartsOption;
-
-  RandomDataset() {
-    this.mergeOptions1 = {
-      dataset: {
-        source: [
-          ['product', '2015', '2016', '2017'],
-          ['Matcha Latte', ...this.getRandomValues()],
-          ['Milk Tea', ...this.getRandomValues()],
-          ['Cheese Cocoa', ...this.getRandomValues()],
-          ['Walnut Brownie', ...this.getRandomValues()],
-        ],
-      },
-    };
-  }
-
-  private getRandomValues() {
-    const res: number[] = [];
-    for (let i = 0; i < 3; i++) {
-      res.push(Math.random() * 100);
-    }
-    return res;
   }
 
   loadRawLines(file: File): Observable<RawLine[]> {
@@ -217,8 +298,8 @@ export class AppComponent implements OnInit {
       /* walk the rows */
       for (let r = range.s.r; r <= range.e.r; ++r) {
         /* find the first cell containing 'VL officielle' */
-        const currentCell = content[utils.encode_cell({ c, r })];
-        if (currentCell?.w === 'VL officielle') {
+        const currCell = content[utils.encode_cell({ c, r })];
+        if (currCell?.w === 'VL officielle') {
           firstRow = r;
           break;
         }
@@ -246,8 +327,8 @@ export class AppComponent implements OnInit {
 
       // check if we got a valid date
       if (date.getTime()) {
-        const value = `${split[2]}/${split[1]}/${split[0]}`;
-        this.data.push(
+        const value = `${split[2]}-${split[1]}-${split[0]}`;
+        this.data?.push(
           this.mapRawLineToIndiceData(rawLine, date.toString(), value)
         );
       }
@@ -265,5 +346,28 @@ export class AppComponent implements OnInit {
       name: date,
       value: [valueXAxis, parseFloat(rawLine.C.replace(',', '.'))],
     };
+  }
+}
+export class CustomValidators {
+  static pattern(regex: RegExp, errorName: string): ValidatorFn {
+    return (control: AbstractControl) =>
+      new RegExp(regex).test(control.value) ? null : { [errorName]: true };
+  }
+
+  static strictlyPositive(control: AbstractControl): ValidationErrors | null {
+    const { value } = control;
+
+    if (value === null || value === undefined) {
+      return null;
+    }
+    if (typeof value !== 'number') {
+      return { isStrictlyPositive: { value } };
+    }
+
+    return value > 0 ? null : { isStrictlyPositive: { value } };
+  }
+
+  static integer(): ValidatorFn {
+    return CustomValidators.pattern(/^-?\d+$/, 'integer');
   }
 }
